18b6fbd02a1a58bab67482a3b4b53d8eee0afc8a
Austin Jones <austin@austinjones.io>
2022-10-15 18:57:37 -0500

    Feat: Implemented FormIoApiParallel

--- ParallelRequestsPOC.App/FormioApiParallel.cs
+++ ParallelRequestsPOC.App/FormioApiParallel.cs
@@ -2,5 +2,19 @@
 
 public class FormioApiParallel
 {
-    
+    // get forms from formio
+    public async Task<HttpResponseMessage[]> Run()
+    {
+        var client = new HttpClient();
+        var responses = new List<Task<HttpResponseMessage>>();
+
+        for (var i = 0; i < 10; i++)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://api.form.io/project/5f9f1b9b9b9b9b9b9b9b9b9b/form");
+            responses.Add(client.SendAsync(request));
+        }
+        var result = await Task.WhenAll(responses);
+        
+        return result;
+    }
 }
